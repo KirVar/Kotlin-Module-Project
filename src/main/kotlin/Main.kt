7684b9f892af7ff2c697fd1dbf624925932ee476
KirVar <el.vartanov@gmail.com>
2024-05-29 21:43:19 +0300

    First commit about console note app

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,20 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
-}
+import Data.createArchive
+import Data.tableOfArchives
+
+fun main() {
+        println("Вас приветствует консольное приложение Заметки")
+        while (true) {
+            when (getCommand(menuMain)) {
+                0 -> createArchive()
+                1 -> {
+                    if (tableOfArchives.isEmpty()) println("Список архивов пуст")
+                    else goToArchiveMenu()
+                }
+
+                2 -> {
+                    println("Будем ждать вас еще, хорошего дня")
+                    break
+                }
+            }
+        }
+    }
